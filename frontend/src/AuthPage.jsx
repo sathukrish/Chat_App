155c2ecedecd2e2b56fcbfa454536eb225431b1f
sathukrish <sathusathushan0530@gmail.com>
2023-07-20 12:43:33 +0530

    path changing

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -5,7 +5,7 @@ const AuthPage = (props) => {
       e.preventDefault();
       const { value } = e.target[0];
       axios.post (
-        'http://localhost:3001/authenticate',
+        'https://chat-app-gne1.onrender.com/authenticate',
         {username: value}
       )
       .then(r => props.onAuth({ ...r.data, secret: value }))
